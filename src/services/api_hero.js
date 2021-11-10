bcfad5f4e812b029bbd451a1c4f7b9d179411b81
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-11-09 20:41:18 -0500

    get api - formater custom

--- src/services/api_hero.js
+++ src/services/api_hero.js
@@ -1,12 +1,25 @@
 import axios from 'axios'
-import { listHero, listSearchHero } from '../redux/heroDucks'
+import { getFormaterHero } from '../helpers/functions'
+import { getResSearch, listHero, listSearchHero } from '../redux/heroDucks'
 
 export const getSuperHero = async (dispatch) => {
   const res = await axios.get(`https://superheroapi.com/api/643351433351950/search/Batman`)
-  dispatch(listHero(res.data.results))
+  const listHeroData = [...res.data.results]
+  const heroes = await getFormaterHero(listHeroData)
+  dispatch(listHero(heroes))
 }
 
 export const getSearchSuperHero = async (text, dispatch) => {
-  const res = await axios.get(`https://superheroapi.com/api/643351433351950/search/${text}`)
-  dispatch(listSearchHero(res.data.results))
+  try {
+    const res = await axios.get(`https://superheroapi.com/api/643351433351950/search/${text}`)
+    if(res.data.response === 'error'){
+      return dispatch(getResSearch(res.data.error))
+    }
+    const listHeroData = [...res.data.results]
+    const listHero = getFormaterHero(listHeroData)
+    dispatch(listSearchHero(listHero))
+    dispatch(getResSearch(''))
+
+  } catch (error) {
+  }
 }
