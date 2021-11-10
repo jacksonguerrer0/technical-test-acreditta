bf7a6a7a78842bd5f70e1ab32ccc81f7eea51ab1
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-11-09 20:56:48 -0500

    icon down and icon up click

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,6 +6,11 @@ import { getSearchSuperHero } from '../services/api_hero';
 
 const Search = () => {
   const [search, setSearch] = useState('Batman');
+  const [iconDown, setIconDown] = useState({
+    habilities: true,
+    appareance: true,
+  })
+
   const heroesStore = useSelector(state => state.heroes)
   const dispatch = useDispatch()
   const handleSearch = (e) => {
@@ -14,10 +19,12 @@ const Search = () => {
   const handleClickSearch = () => {
     getSearchSuperHero(search, dispatch)
   }
-  const handleHabilitiesOrder = () => {
+  const handleHabilitiesOrder = (e) => {
+    setIconDown({...iconDown, habilities: !(iconDown.habilities)})
     dispatch(habilitiesOrderAZ(!(heroesStore.habilitiesOrderAZ)))
   }
   const handleAppearanceOrder = () => {
+    setIconDown({...iconDown, appareance: !(iconDown.appareance)})
     dispatch(appearanceOrderAZ(!(heroesStore.appearanceOrderAZ)))
   }
   return (
@@ -31,8 +38,8 @@ const Search = () => {
         alignRight
         onClick={handleClickSearch}
       >
-        <Dropdown.Item onClick={handleHabilitiesOrder}>Habilidades <i className="bi bi-chevron-double-up"></i></Dropdown.Item>
-        <Dropdown.Item onClick={handleAppearanceOrder}>Apariencia <i className="bi bi-chevron-double-down"></i></Dropdown.Item>
+        <Dropdown.Item onClick={handleHabilitiesOrder}>Habilidades <i className={`bi ${iconDown.habilities? 'bi-chevron-double-down':'bi-chevron-double-up'}`}></i></Dropdown.Item>
+        <Dropdown.Item onClick={handleAppearanceOrder}>Apariencia <i className={`bi ${iconDown.appareance? 'bi-chevron-double-down':'bi-chevron-double-up'}`}></i></Dropdown.Item>
       </SplitButton>
     </InputGroup>
     </div>
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import ListHero from '../components/ListHero'
 import Search from '../components/Search'
 import { getSuperHero } from '../services/api_hero'
